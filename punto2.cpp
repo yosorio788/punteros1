f36393bf17d2945be7adc79830160b52b110d013
yosorio788 <37355564+yosorio788@users.noreply.github.com>
2019-03-04 18:49:46 -0500

    Update punto2.cpp

--- punto2.cpp
+++ punto2.cpp
@@ -1,24 +1,37 @@
+/*Diseñe un programa, que sume dos variables de tipo entero, 
+por medio de apuntadores, debe mostrar el resultado 
+y la dirección de memoria de 
+cada uno de los valores incluido el resultado.
+*/
 
-
-#include<iostream>
-#include<conio.h>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-		int a,b,suma;
-		int *pa,*pb,*psuma;
 	
-		cout<<"Digite un numero1 : ";
-		cin>>a;
-		
-		cout<<"Digite un numero 2: ";
-		cin>>b;
-		
-		suma=a+b;
+	int num1,num2,sum;
+	int *p_num1, *p_num2, *p_suma;
 	
-	cout<<"la posicion de memoria del # 1 es: "<<&a<<endl;
-	cout<<"la posicion de memoria del # 2 es "<<&b<<endl;
-	cout<<"la posicion de memoria de la suma es: "<<&suma<<endl;
+	printf("INGRESE EL PRIMER NUMERO: \n");
+	scanf("%d",&num1);
+	
+	printf("INGRESE EL SEGUNDO NUMERO: \n");
+	scanf("%d",&num2);
+	
+		sum= num1+num2;
+	
+	printf("LA SUMA DE LAS DOS VARIABLES ES: %d \n",sum);
 
+		p_num1=&num1;
+		p_num2=&num2;
+	
+	printf("SUMA DE LOS DOS PUNTEROS%d\n",*p_num1+*p_num2);
+	
+		p_suma =&sum;
+	
+	printf("LA DIRECCION DE MEMORIA DEL PRIMER NUMERO :%p\n",p_num1);
+	printf("LA DIRECCION DE MEMORIA DEL SEGUNDO NUMERO:%p\n",p_num2);
+	printf("LA POSICION DE LA SUMA ES:%p\n\n",p_suma);
+	
 	return 0;
 }
